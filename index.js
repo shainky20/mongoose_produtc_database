9a96cdf9b4541d6d235549773534ae984d014739
shainky20 <shainkysoni47@gmail.com>
2026-03-06 22:36:03 +0530

    fix index server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import { connectDB } from "./config/db.js";
-const app = express();
+import app from "./src/app.js";
+import connectDb from "./src/config/db.js";
 
-app.use(express.json());
-connectDB()
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => 
-    console.log("Server is running on port 3000")
-);
+connectDb();
+
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
